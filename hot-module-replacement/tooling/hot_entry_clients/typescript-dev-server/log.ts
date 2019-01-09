15c37a5a57bfa662465cdd6743f5c00546d020fa
Alex Leung <alex.l.leung@gmail.com>
2019-01-08 23:12:01 -0800

    forgot to add these to prior commit

---
+++ hot-module-replacement/tooling/hot_entry_clients/typescript-dev-server/log.ts
@@ -0,0 +1,42 @@
+var logLevel = "info";
+
+function dummy() {}
+
+function shouldLog(level) {
+	var shouldLog =
+		(logLevel === "info" && level === "info") ||
+		(["info", "warning"].indexOf(logLevel) >= 0 && level === "warning") ||
+		(["info", "warning", "error"].indexOf(logLevel) >= 0 && level === "error");
+	return shouldLog;
+}
+
+function logGroup(logFn) {
+	return function(level, msg) {
+		if (shouldLog(level)) {
+			logFn(msg);
+		}
+	};
+}
+
+export const log = function(level, msg) {
+	if (shouldLog(level)) {
+		if (level === "info") {
+			console.log(msg);
+		} else if (level === "warning") {
+			console.warn(msg);
+		} else if (level === "error") {
+			console.error(msg);
+		}
+	}
+};
+
+var group = console.group || dummy;
+var groupCollapsed = console.groupCollapsed || dummy;
+var groupEnd = console.groupEnd || dummy;
+
+log.group = logGroup(group);
+log.groupCollapsed = logGroup(groupCollapsed);
+log.groupEnd = logGroup(groupEnd);
+log.setLogLevel = function(level) {
+	logLevel = level;
+};
